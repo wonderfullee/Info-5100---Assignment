b7b4f7bb1737527a7d7539c4fd52d9f8e0498213
wonderfullee <35618935+wonderfullee@users.noreply.github.com>
2018-04-11 22:10:16 -0700

    Delete StudentsPersistor.java

--- Assignment_8/class9/StudentsPersistor.java
+++
@@ -1,7 +0,0 @@
-package class10.Assignment_8.class9;
-
-public interface StudentsPersistor {
-    void save(StudentsManager manager) throws Exception;
-
-    StudentsManager load() throws Exception;
-}
